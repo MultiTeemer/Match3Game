da69936729a142c679e26a52f165e411415194bf
monoklid <artem94@bk.ru>
2015-06-28 16:51:46 +1000

    Use delegates for updating/drawing screens

--- Match3Game.cs
+++ Match3Game.cs
@@ -15,6 +15,8 @@ namespace GameForest_Test_Task
         SpriteBatch spriteBatch;
         CurrentScreenE curScreen;
 
+        private delegate void updater(GameTime gameTime);
+        private delegate void drawer(GameTime gameTime);
 
         private string[] requiredTextures;
         private Dictionary<string, Texture2D> textures;
@@ -61,6 +63,9 @@ namespace GameForest_Test_Task
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+            updater[] updaters = { this.updateMainMenuScreen, this.updateGameScreen };
+
+            updaters[(int)this.curScreen](gameTime);
 
             base.Update(gameTime);
         }
@@ -69,38 +74,62 @@ namespace GameForest_Test_Task
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            spriteBatch.Begin();
+            spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.NonPremultiplied);
 
-            switch (curScreen)
-            {
-                case CurrentScreenE.MainMenuScreen:
-                    int btnCenterX = (GraphicsDevice.Viewport.Width - playBtn.Width) / 2;
-                    int btnCenterY = (GraphicsDevice.Viewport.Height - playBtn.Height) / 2;
+            drawer[] drawers = { this.drawMainMenuScreen, this.drawGameScreen };
 
-                    Point mp = getMousePosition();
-                    Rectangle btnRect = new Rectangle(btnCenterX, btnCenterY, playBtn.Width, playBtn.Height);
-
-                    Texture2D btnToDraw = playBtn;
-
-                    if (btnRect.Contains(mp))
-                    {
-                        btnToDraw = Mouse.GetState().LeftButton == ButtonState.Pressed ? playBtnClicked : playBtnHov;
-                    }
-            Texture2D playBtn = textures["playBtn"];
-
-                    spriteBatch.Draw(btnToDraw, new Vector2(btnCenterX, btnCenterY), Color.White);
-
-                    break;
-                case CurrentScreenE.GameScreen:
-
-                    break;
-            }
+            drawers[(int)curScreen](gameTime);
 
             spriteBatch.End();
-            Texture2D playBtn = textures["playBtn"];
-                btnToDraw = Mouse.GetState().LeftButton == ButtonState.Pressed ? textures["playBtnClicked"] : textures["playBtnHov"];
 
             base.Draw(gameTime);
+        }
+
+        private void updateMainMenuScreen(GameTime gameTime)
+        {
+            Texture2D playBtn = textures["playBtn"];
+
+            int btnCenterX = (GraphicsDevice.Viewport.Width - playBtn.Width) / 2;
+            int btnCenterY = (GraphicsDevice.Viewport.Height - playBtn.Height) / 2;
+
+            Point mp = getMousePosition();
+            Rectangle btnRect = new Rectangle(btnCenterX, btnCenterY, playBtn.Width, playBtn.Height);
+
+            if (btnRect.Contains(mp) && Mouse.GetState().LeftButton == ButtonState.Pressed)
+            {
+                curScreen = CurrentScreenE.GameScreen;
+                gameState = GameStatusE.GameRunning;
+            }
+        }
+
+        private void drawMainMenuScreen(GameTime gameTime)
+        {
+            Texture2D playBtn = textures["playBtn"];
+
+            int btnCenterX = (GraphicsDevice.Viewport.Width - playBtn.Width) / 2;
+            int btnCenterY = (GraphicsDevice.Viewport.Height - playBtn.Height) / 2;
+
+            Point mp = getMousePosition();
+            Rectangle btnRect = new Rectangle(btnCenterX, btnCenterY, playBtn.Width, playBtn.Height);
+
+            Texture2D btnToDraw = playBtn;
+
+            if (btnRect.Contains(mp))
+            {
+                btnToDraw = Mouse.GetState().LeftButton == ButtonState.Pressed ? textures["playBtnClicked"] : textures["playBtnHov"];
+            }
+
+            spriteBatch.Draw(btnToDraw, new Vector2(btnCenterX, btnCenterY), Color.White);
+        }
+
+        private void updateGameScreen(GameTime gameTime)
+        {
+
+        }
+
+        private void drawGameScreen(GameTime gameTime)
+        {
+
         }
 
         private Point getMousePosition()
