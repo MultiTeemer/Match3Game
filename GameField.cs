85e1d5ac95642505b8853ab29ebb4edab973309d
monoklid <artem94@bk.ru>
2015-06-29 12:23:46 +1000

    Add game field infastructure

---
+++ GameField.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameForest_Test_Task
+{
+    struct GameField
+    {
+        public enum BlockTypeE
+        { 
+            Cirle,
+            Diamond,
+            Hex,
+            Square,
+            Star,
+            Triangle,
+
+            BlocksCount,
+        };
+
+        public BlockTypeE[] field;
+
+        public GameField(int size)
+        {
+            field = new BlockTypeE[size * size];
+        }
+
+        public void Init()
+        {
+            Random gen = new Random();
+
+            for (int i = 0; i < field.Length; ++i)
+            {
+                field[i] = (BlockTypeE)gen.Next(0, (int)BlockTypeE.BlocksCount - 1);
+            }
+        }
+
+        public void Swap(int idx1, int idx2)
+        {
+            BlockTypeE tmp = field[idx1];
+            field[idx1] = field[idx2];
+            field[idx2] = tmp;
+        }
+    }
+}
--- Match3Game.cs
+++ Match3Game.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
 
 namespace GameForest_Test_Task
 {
@@ -11,17 +12,38 @@ namespace GameForest_Test_Task
             MainMenuScreen,
             GameScreen,
         };
+
+        private enum GameStatusE
+        {
+            GameRunning,
+            GameStoped,
+        };
+
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+
         CurrentScreenE curScreen;
 
         MouseState lastState;
+
+        GameStatusE gameState;
+        GameTime gameStartTime;
+        GameField field;
 
         private delegate void updater(GameTime gameTime);
         private delegate void drawer(GameTime gameTime);
 
         private string[] requiredTextures;
         private Dictionary<string, Texture2D> textures;
+        private Texture2D[] blocksTextures;
+
+        private const int FIELD_SIZE = 8;
+
+        private int FIELD_SHIFT_BY_X;
+        private int FIELD_SHIFT_BY_Y;
+        private int BLOCK_TEXTURE_SIZE;
+
+        private int curSelectedItemIdx;
 
         public Match3Game()
         {
@@ -36,9 +58,27 @@ namespace GameForest_Test_Task
                 "playBtn",
                 "playBtnHov",
                 "playBtnClicked",
+                "circle",
+                "diamond",
+                "hex",
+                "square",
+                "star",
+                "triangle",
+                "circleHl",
+                "diamondHl",
+                "hexHl",
+                "squareHl",
+                "starHl",
+                "triangleHl",
             };
 
             textures = new Dictionary<string, Texture2D>();
+
+            field = new GameField(FIELD_SIZE);
+
+            blocksTextures = new Texture2D[(int)GameField.BlockTypeE.BlocksCount * 2];
+
+            curSelectedItemIdx = -1;
         }
 
         protected override void Initialize()
@@ -54,6 +94,27 @@ namespace GameForest_Test_Task
             {
                 textures.Add(requiredTextures[i], Content.Load<Texture2D>("graphics/" + requiredTextures[i]));
             }
+
+            string[] blocksTexturesNames = new string[] {
+                "circle",
+                "diamond",
+                "hex",
+                "square",
+                "star",
+                "triangle",
+            };
+
+            int blocksCount = (int)GameField.BlockTypeE.BlocksCount;
+
+            for (int i = 0; i < blocksCount; ++i)
+            {
+                blocksTextures[i] = textures[blocksTexturesNames[i]];
+                blocksTextures[i + blocksCount] = textures[blocksTexturesNames[i] + "Hl"];
+            }
+
+            BLOCK_TEXTURE_SIZE = blocksTextures[0].Width;
+            FIELD_SHIFT_BY_X = (GraphicsDevice.Viewport.Width - FIELD_SIZE * BLOCK_TEXTURE_SIZE) / 2;
+            FIELD_SHIFT_BY_Y = (GraphicsDevice.Viewport.Height - FIELD_SIZE * BLOCK_TEXTURE_SIZE) / 2;
         }
 
         protected override void UnloadContent()
@@ -99,10 +160,13 @@ namespace GameForest_Test_Task
             Point mp = getMousePosition();
             Rectangle btnRect = new Rectangle(btnCenterX, btnCenterY, playBtn.Width, playBtn.Height);
 
-            if (btnRect.Contains(mp) && Mouse.GetState().LeftButton == ButtonState.Released && lastState.LeftButton == ButtonState.Pressed)
+            if (btnRect.Contains(mp) && leftKeyClick())
             {
                 curScreen = CurrentScreenE.GameScreen;
                 gameState = GameStatusE.GameRunning;
+                gameStartTime = gameTime;
+
+                field.Init();
             }
         }
 
@@ -128,17 +192,44 @@ namespace GameForest_Test_Task
 
         private void updateGameScreen(GameTime gameTime)
         {
+            int gameFieldSideLength = FIELD_SIZE * BLOCK_TEXTURE_SIZE;
+            Rectangle gameFieldRect = new Rectangle(FIELD_SHIFT_BY_X, FIELD_SHIFT_BY_Y, gameFieldSideLength, gameFieldSideLength);
+            Point mp = getMousePosition();
 
+            if (gameFieldRect.Contains(mp) && leftKeyClick())
+            {
+                curSelectedItemIdx = (mp.Y - FIELD_SHIFT_BY_Y) / BLOCK_TEXTURE_SIZE * FIELD_SIZE + (mp.X - FIELD_SHIFT_BY_X) / BLOCK_TEXTURE_SIZE;
+            }
         }
 
         private void drawGameScreen(GameTime gameTime)
         {
+            int blocksCount = (int)GameField.BlockTypeE.BlocksCount;
 
+            for (int i = 0; i < FIELD_SIZE; ++i)
+            {
+                for (int j = 0; j < FIELD_SIZE; ++j)
+                {
+                    int idx1d = i * FIELD_SIZE + j;
+
+                    Texture2D blockTexture = blocksTextures[(int)field.field[idx1d] + (idx1d == curSelectedItemIdx ? blocksCount : 0)];
+
+                    int x = FIELD_SHIFT_BY_X + j * BLOCK_TEXTURE_SIZE;
+                    int y = FIELD_SHIFT_BY_Y + i * BLOCK_TEXTURE_SIZE;
+
+                    spriteBatch.Draw(blockTexture, new Vector2(x, y), Color.White);
+                }
+            }
         }
 
         private Point getMousePosition()
         {
             return Mouse.GetState().Position;
+        }
+
+        private bool leftKeyClick()
+        {
+            return Mouse.GetState().LeftButton == ButtonState.Released && lastState.LeftButton == ButtonState.Pressed;
         }
     }
 }
